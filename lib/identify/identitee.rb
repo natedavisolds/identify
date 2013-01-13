732ab7dd36f18aac20eb1f477ed937dfcb943df2
Nate Davis Olds <nate@innateservices.com>
2013-01-12 23:44:06 -0500

    retains knowledge of the instance; defines the instance through a block

--- lib/identify/identitee.rb
+++ lib/identify/identitee.rb
@@ -1,9 +1,20 @@
+require 'identify/identifiables'
+
 module Identify
   module Identitee
-    def identify
+    def identify id, *extra_parameters, &block
+      @identifiables ||= Identifiables.new
+
+      instance = new *extra_parameters
+      instance.instance_eval &block if block_given?
+      @identifiables.register id, instance
+      instance
     end
 
-    def find_identifiable
+    def find_identifiable candidate_id
+      @identifiables ||= Identifiables.new
+
+      @identifiables.find candidate_id
     end
   end
 end
--- spec/lib/identify/identify_spec.rb
+++ spec/lib/identify/identify_spec.rb
@@ -1,19 +1,46 @@
 require 'identify'
 
 describe Identify do
-  it "exposes an identify method" do
-    klass = Class.new do
+  it "registers the identifiable" do
+    TestIdentifyRegister = Class.new do
       include Identify
     end
 
-    klass.should respond_to :identify
+    instance = TestIdentifyRegister.identify :test
+
+    TestIdentifyRegister.find_identifiable(:test).should == instance
   end
 
-  it "exposes an identify method" do
-    klass = Class.new do
+  it "passes extra parameters through to new" do
+    TestIdentifyMultipleParams = Class.new do
       include Identify
+
+      attr_reader :attrib
+
+      def initialize attrib
+        @attrib = attrib
+      end
     end
 
-    klass.should respond_to :find_identifiable
+    instance = TestIdentifyMultipleParams.identify :test, "Testing"
+    instance.attrib.should == "Testing"
+  end
+
+  it "passes the block to the new as well" do
+    TestIdentifyPassingBlock = Class.new do
+      include Identify
+
+      attr_reader :attrib
+
+      def set_attrib str
+        @attrib = str
+      end
+    end
+
+    instance = TestIdentifyPassingBlock.identify :test do
+      set_attrib "It ran the block!"
+    end
+
+    instance.attrib.should == "It ran the block!"
   end
 end
